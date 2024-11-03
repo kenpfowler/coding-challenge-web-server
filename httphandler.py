49807774f5e28331479c3ca7fbf496956570451a
Ken Fowler <development@kennyf.ca>
2024-11-03 14:44:38 -0500

    feat: http handler

---
+++ httphandler.py
@@ -0,0 +1,41 @@
+
+from models.request import Request
+from models.response import Response
+# http message handler
+# try
+# parse arbitrary data sent to the server as html request
+# if data cannot be parsed as an http request send 400 error
+# otherwise, respond with 200 and send the index page 
+# or 404 and send the not found page if any path other than root is used
+
+def http_message_handler(message):
+    try:
+        request = Request(message)
+        url = request.message["path"]
+
+        match url:
+            case "/":
+                with open("./www/index.html") as file:
+                    content = file.read()
+                    length = len(content)
+
+                    server_response = Response("HTTP/1.1", "200", "OK", content)
+                    server_response.headers["Content-Type:"] = "text/html; charset=UTF-8"
+                    server_response.headers["Connection:"] = "close"
+                    server_response.headers["Content-Length:"] = str(length)
+                    return server_response
+            case _:
+                    with open("./www/not-found.html") as file:
+                        content = file.read()
+                        length = len(content)
+
+                        server_response = Response("HTTP/1.1", "404", "Not Found", content)
+                        server_response.headers["Content-Type:"] = "text/html; charset=UTF-8"
+                        server_response.headers["Connection:"] = "close"
+                        server_response.headers["Content-Length:"] = str(length)
+                        return server_response        
+    except:
+        server_response = Response("HTTP/1.1", "400", "Bad Request", "")
+        server_response.headers["Connection:"] = "close"
+        return server_response
+
